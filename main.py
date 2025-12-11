9500d1b05c751d508f6db99d20ec8083b92bf0e1
Nikita <ntarasov0326@icloud.com>
2025-12-11 15:58:36 +0300

    Fixed user input, a small refactoring of functions

--- main.py
+++ main.py
@@ -3,19 +3,30 @@ import random
 def separator():
     print("-" * 30)
 
-def user_input(request) -> int:
-    temp = input(request)
-    while not temp.isdigit():
-        print("Please, type a number next time.")
-        temp = input(request)
-    temp = int(temp)
-    while temp <= 0:
-        print("Please, type a number larger then 0 next time.")
-        temp = int(input(request))
-    return temp
+def get_user_input(request: str) -> int:
 
-def play_round(random_number) -> bool:
-    user_guess = user_input("Make a guess: ")
+    while True:
+        user_input = input(request).strip()
+        if user_input != user_input.strip("-") or user_input == '0':
+            print("Please, type a number larger than 0 next time.")
+            continue
+        try:
+            return int(user_input)
+        except ValueError:
+            print("Please, type a number next time.")
+
+    # temp = input(request)
+    # while not temp.isdigit():
+    #     print("Please, type a number next time.")
+    #     temp = input(request)
+    # temp = int(temp)
+    # while temp <= 0:
+    #     print("Please, type a number larger then 0 next time.")
+    #     temp = int(input(request))
+    # return temp
+
+def play_round(random_number: int) -> bool:
+    user_guess = get_user_input("Make a guess: ")
 
     if user_guess == random_number:
         separator()
@@ -32,7 +43,7 @@ def main() -> None:
     print("Hello, Welcome to the my game!")
     separator()
 
-    top_of_range = user_input("Type a number: ")
+    top_of_range = get_user_input("Type a number: ")
 
     random_number = random.randint(0, top_of_range)
     guesses = 0
