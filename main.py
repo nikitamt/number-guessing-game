e197c4816c64ca30853f36e493004b453f25f7e2
Nikita <ntarasov0326@icloud.com>
2025-12-11 15:29:01 +0300

    Added functions: main, play_round, user_input, separator

--- main.py
+++ main.py
@@ -1,33 +1,49 @@
 import random
 
-top_of_range = input("Type a number: ");
+def separator():
+    print("-" * 30)
 
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
-    if top_of_range <= 0:
-        print("Please, type a number larger than 0 next time.")
-        quit()
-else:
-    print("Please, type a number next time.")
-    quit()
-
-random_number = random.randint(0, top_of_range)
-guesses = 0
-
-while True:
-    guesses += 1
-    user_guess = input("Make a guess: ")
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
-    else:
+def user_input(request) -> int:
+    temp = input(request)
+    while not temp.isdigit():
         print("Please, type a number next time.")
-        continue
+        temp = input(request)
+    temp = int(temp)
+    while temp <= 0:
+        print("Please, type a number larger then 0 next time.")
+        temp = int(input(request))
+    return temp
+
+def play_round(random_number) -> bool:
+    user_guess = user_input("Make a guess: ")
 
     if user_guess == random_number:
+        separator()
         print("You got it!")
-        break
+        return True
     elif user_guess < random_number:
         print("You were above the number!")
     elif user_guess > random_number:
         print("You were below the number!")
-print(f"You got it in {guesses} guesses")
+    return False
+
+
+def main() -> None:
+    print("Hello, Welcome to the my game!")
+    separator()
+
+    top_of_range = user_input("Type a number: ")
+
+    random_number = random.randint(0, top_of_range)
+    guesses = 0
+
+    while True:
+        guesses += 1
+        if play_round(random_number):
+            break
+
+    print(f"You got it in {guesses} guesses.")
+    return
+
+if __name__ == "__main__":
+    main()
